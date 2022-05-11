f917630496a52d3017cc0d1bc7fbdf5e24699a67
MajaDit <marie.ditrtova@gmail.com>
2022-05-11 15:20:06 +0100

    created css file for ExpenseDate component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,12 +1,14 @@
+import "./ExpenseDate.css";
+
 function ExpenseDate(props) {
   const month = props.dates.toLocaleString("en-GB", { month: "long" });
   const year = props.dates.getFullYear();
   const day = props.dates.toLocaleString("en-GB", { day: "2-digit" });
   return (
-    <div className="ExpenseDate">
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 }

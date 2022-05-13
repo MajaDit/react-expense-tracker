fa5e423c3e4aed95d420904e6faeb4e72fecf317
MajaDit <marie.ditrtova@gmail.com>
2022-05-13 20:15:28 +0100

    wrapped ExpenseDate.js in Card

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,15 +1,16 @@
 import "./ExpenseDate.css";
+import Card from "./Card";
 
 function ExpenseDate(props) {
   const month = props.dates.toLocaleString("en-GB", { month: "long" });
   const year = props.dates.getFullYear();
   const day = props.dates.toLocaleString("en-GB", { day: "2-digit" });
   return (
-    <div className="expense-date">
+    <Card className="expense-date">
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__year">{year}</div>
       <div className="expense-date__day">{day}</div>
-    </div>
+    </Card>
   );
 }
 

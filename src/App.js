242787ce76310c14ad5e7ec6c8de3c4a9e69e152
MajaDit <marie.ditrtova@gmail.com>
2022-06-16 19:04:10 +0100

    created an new expenses component with expense form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -29,7 +30,7 @@ const App = () => {
   ];
   return (
     <div>
-      <h2>Expense Tracker</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );

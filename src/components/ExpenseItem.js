7e73ab363b117cb984ea4b41c168f351f8a23942
Maja Dit <maja.ditrtova@outlook.com>
2022-08-28 17:02:44 +0100

    added filter on expense items

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,10 +7,9 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate dates={props.date} />
       <div className="expense-item__description">
-        <h2>Title</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button>Button</button>
     </Card>
   );
 };
